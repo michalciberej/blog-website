c4c9f16b4137d571f65f234d834a4e7769f2d21f
Michal Ciberej <michal.ciberej@gmail.com>
2024-03-27 00:01:55 +0200

    Add test category

--- app/content/postCategories.ts
+++ app/content/postCategories.ts
@@ -1,3 +1,3 @@
-export const categories = ['Test1', 'Test2', 'Test3'] as const;
+export const categories = ['Test1', 'Test2', 'Test3', 'Test4'] as const;
 
 export type Category = (typeof categories)[number];
