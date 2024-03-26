2b3f0b25c9906ed4cfc395ac8fec365f3f34cd1a
Michal Ciberej <michal.ciberej@gmail.com>
2024-03-27 00:02:09 +0200

    Fix import

--- app/(pages)/posts/[slug]/page.tsx
+++ app/(pages)/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { ContentSidebar, PostLayout, PostHeading } from '@/app/components';
 import styles from './page.module.scss';
-import getPostBySlug from '@/lib/getPostBySlug';
+import getPostBySlug from '@/app/lib/getPostBySlug';
 import Mdx from '@/app/components/MDX/mdx-components';
 import '@/app/scss/code-highlight.scss';
 
