737d7281224a3f40fddc90859374062e77dbc407
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-03 00:11:25 +0200

    Add PreviewGroup on end of every PostPage

--- app/(pages)/posts/[slug]/page.tsx
+++ app/(pages)/posts/[slug]/page.tsx
@@ -1,8 +1,15 @@
-import { ContentSidebar, PostLayout, PostHeading } from '@/app/components';
+import {
+  ContentSidebar,
+  PostLayout,
+  PostHeading,
+  PreviewGroup,
+} from '@/app/components';
 import styles from './page.module.scss';
 import getPostBySlug from '@/app/lib/getPostBySlug';
 import Mdx from '@/app/components/MDX/mdx-components';
+import { allPosts, Post } from '@/.contentlayer/generated';
 import '@/app/scss/code-highlight.scss';
+import getRandomPosts from '@/app/lib/getRandomPosts';
 
 interface PostPageProps {
   params: {
@@ -10,19 +17,29 @@ interface PostPageProps {
   };
 }
 
+export const generateStaticParams = () => {
+  return allPosts.map((post: Post) => {
+    slug: post.slugAsParams;
+  });
+};
+
 const PostPage = async ({ params }: PostPageProps) => {
   const post = await getPostBySlug(params.slug);
+  const randomPosts = getRandomPosts(params.slug, 3);
 
   return (
-    <>
-      <div className={styles.container}>
-        <PostLayout>
-          <PostHeading post={post} />
-          <Mdx code={post.body.code} />
-        </PostLayout>
-        <ContentSidebar post={post} />
-      </div>
-    </>
+    <div className={styles.container}>
+      <PostLayout>
+        <PostHeading post={post} />
+        <Mdx code={post.body.code} />
+        <PreviewGroup
+          posts={randomPosts}
+          title='Other Posts'
+          amount={3}
+        />
+      </PostLayout>
+      <ContentSidebar post={post} />
+    </div>
   );
 };
 
