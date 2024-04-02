8e8567bb927f11d0d285215b4dbfcef9eaba27f1
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-03 00:10:26 +0200

    Add content/styles

---
+++ app/(pages)/posts/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import styles from './page.module.scss';
+import { categories, Category } from '@/app/content/postCategories';
+import { useEffect, useState } from 'react';
+import { PreviewGroup } from '@/app/components';
+import getRecentPosts from '@/app/lib/getRecentPosts';
+
+const colorPrimary = '#4654a5';
+
+const AllPostsWebsite = () => {
+  const allPosts = getRecentPosts();
+  const [currentCategory, setCurrentCategory] = useState<Category>('Recent');
+  const [currentAmount, setCurrentAmount] = useState(9);
+  const [posts, setPosts] = useState(allPosts);
+
+  const handleSort = (c: Category) => {
+    setCurrentCategory(c);
+  };
+
+  const handleShowMore = () => {
+    setCurrentAmount(currentAmount + 9);
+  };
+
+  useEffect(() => {
+    if (currentCategory === 'Recent') setPosts(allPosts);
+    else setPosts(allPosts.filter((post) => post.category === currentCategory));
+  }, [currentCategory, allPosts]);
+
+  return (
+    <div className={styles.page}>
+      <ul className={styles.sortList}>
+        {categories.map((category, index) => (
+          <li key={index}>
+            <button
+              className={styles.sortEl}
+              type='button'
+              style={{
+                borderColor:
+                  currentCategory === category ? colorPrimary : undefined,
+              }}
+              onClick={() => handleSort(category)}
+              aria-label={`Sort by ${category}`}>
+              {category}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <section>
+        <h2>All Posts</h2>
+        {posts && (
+          <PreviewGroup
+            posts={posts}
+            amount={currentAmount}
+          />
+        )}
+      </section>
+      {currentAmount < allPosts.length && (
+        <button onClick={handleShowMore}>Show More</button>
+      )}
+    </div>
+  );
+};
+
+export default AllPostsWebsite;
