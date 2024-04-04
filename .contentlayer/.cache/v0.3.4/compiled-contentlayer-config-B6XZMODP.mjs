86b6b193066224e479fa14d990f73a4f6a95ff68
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-04 19:56:47 +0200

    Add content

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-B6XZMODP.mjs
@@ -0,0 +1,84 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+var computedFields = {
+  slug: { type: "string", resolve: (post) => `/${post._raw.flattenedPath}` },
+  slugAsParams: {
+    type: "string",
+    resolve: (post) => post._raw.flattenedPath.split("/").slice(1).join("/")
+  }
+};
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "content/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    publishDate: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    category: {
+      type: "string",
+      required: true
+    },
+    content: {
+      type: "string",
+      required: false
+    },
+    difficulty: {
+      type: "string",
+      required: true
+    },
+    readTime: {
+      type: "number",
+      required: true
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "app",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          }
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheadings-anchor"],
+            ariaLabel: "link to section"
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-B6XZMODP.mjs.map
---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-ICOKLJX7.mjs
@@ -0,0 +1,82 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+var computedFields = {
+  slug: { type: "string", resolve: (post) => `/${post._raw.flattenedPath}` },
+  slugAsParams: {
+    type: "string",
+    resolve: (post) => post._raw.flattenedPath.split("/").slice(1).join("/")
+  }
+};
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "content/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    publishDate: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    category: {
+      type: "string",
+      required: true
+    },
+    content: {
+      type: "string",
+      required: false
+    },
+    difficulty: {
+      type: "string",
+      required: true
+    },
+    readTime: {
+      type: "number",
+      required: true
+    },
+    liveDemo: {
+      type: "string",
+      required: false
+    },
+    sourceCode: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "app",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-ICOKLJX7.mjs.map
